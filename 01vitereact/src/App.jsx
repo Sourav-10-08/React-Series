b3c8696558088ef00269b5f642dd2e7d0917ceb9
Sourav Roy <sroy69645@gmail.com>
2025-01-17 02:04:29 +0530

    ss

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,15 @@
 import Chai from "./chai";
 
 function App() {
+      
+  const username = "Sourav Roy"
 
-
-  return (
+  return ( 
+  <>
+    <h1>My name is {username}</h1>
    <Chai/>
+
+   </>
   )
 }
 
