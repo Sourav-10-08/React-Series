23644b00110265b308678526363c6b9f169eb142
My Projects <sroy69645@gmail.com>
2025-11-04 22:16:00 +0530

    fix the error in bgchanger project and revise the concept

---
+++ 04bgchanger_project/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcss from "@tailwindcss/vite";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
