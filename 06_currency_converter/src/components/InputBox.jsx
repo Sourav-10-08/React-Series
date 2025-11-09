ea2b79f89d31436737d63979b661c1dc1836e829
My Projects <sroy69645@gmail.com>
2025-11-09 21:59:52 +0530

    added input box for currency converter

---
+++ 06_currency_converter/src/components/InputBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrenyChange,
+  currencyOption = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = fa lse
+ className = "",}) 
+ {
+    
+
+
+  return (
+    <div className= {` `}>
+      
+    </div>
+  )
+}
+
+export default InputBox
